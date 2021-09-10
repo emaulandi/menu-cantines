2611c2a27f31e2d558f98cd4b3aad2d63b7740fb
emaulandi <e.maulandi@gmail.com>
2021-09-10 14:15:34 +0200

    Center svg in AlimentRadial

--- src/components/AlimentRadial.jsx
+++ src/components/AlimentRadial.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect } from 'react';
 
 import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 import initRadial from '../radial';
 import weeksSaison from '../data/weeks_saison.json';
 
+const useStyles =  makeStyles({
+  svgContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+});
+
 const AlimentRadial = ({ svgId, svgSide, aliment, label }) => {
+  const classes = useStyles();
+
   useEffect(() => {
     const dataWeeks = weeksSaison
       .filter(({ match }) => match === aliment)
@@ -15,7 +25,7 @@ const AlimentRadial = ({ svgId, svgSide, aliment, label }) => {
   });
 
   return (
-    <Box>
+    <Box className={classes.svgContainer}>
       <svg id={svgId}> </svg>
     </Box>
   );
